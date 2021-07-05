34b386334b91188ff41b8f4b63146dd3a08de57e
Kerman <KermanX@foxmail.com>
2021-07-05 16:39:43 +0800

    update Core

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -1,2 +1,18 @@
 #include "Core.h"
 
+template<int ColNum, int RowNum>
+inline bool ChessDataImpl<ColNum, RowNum>::init()
+{
+	for (auto row : data) {
+		for (auto grid : row) {
+			grid
+		}
+	}
+	return false;
+}
+
+template<int ColNum, int RowNum>
+Player ChessDataImpl<ColNum, RowNum>::WhoWins()
+{
+	return Player();
+}
--- Core/Core.h
+++ Core/Core.h
@@ -1,1 +1,67 @@
 #pragma once
+#include <vector>
+#include <array>
+#include <memory>
+
+enum class Player {
+	None,
+	P1,
+	P2
+};
+
+enum class Direction {
+	Left = 1,
+	Right = 2,
+	Top = 4,
+	Bottom = 8,
+};
+
+class ChessGrid {
+public:
+	ChessGrid() = default;
+};
+
+class ChessGridHome :public ChessGrid {
+public:
+	ChessGridHome() = default;
+protected:
+	Player whose = Player::None;
+	Direction direction;
+};
+
+class ChessGridNormal :public ChessGrid {
+public:
+	struct{
+		struct{
+			Player whose;
+		}Left,Top,Right,Bottom;
+		struct{
+			enum class CrossType {
+				None,
+				LeftTop_RightBottom,
+				LeftBottom_RightTop
+			}Type;
+			Player whose;
+		}Cross;
+	}Mirror;
+};
+
+class ChessData {
+public:
+	ChessData() = default;
+public:
+	virtual bool init() = 0;
+	virtual Player WhoWins() = 0;
+};
+
+template <int ColNum, int RowNum>
+class ChessDataImpl :public ChessData {
+protected:
+	using RowType = std::array<ChessGrid*, ColNum>;
+	using DataType = std::array<RowType, RowNum>;
+	DataType data;
+public:
+	virtual bool init() override;
+	virtual Player WhoWins() override;
+
+};
