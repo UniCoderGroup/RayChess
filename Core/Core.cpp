5dc9cd1076a353f15fc042ccd9edea79d1e7c716
Kerman <KermanX@foxmail.com>
2021-07-06 12:45:04 +0800

    Update Core

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -1,23 +1,71 @@
 #include "Core.h"
 
-template<int ColNum, int RowNum>
-inline bool ChessDataImpl<ColNum, RowNum>::init()
+GridType ChessGridHome::GetGridType()
+{
+	return GridType::Home;
+}
+
+GridType ChessGridNormal::GetGridType()
+{
+	return GridType::Normal;
+}
+
+bool ChessGridNormal::AddMirror(int x, int y, MirrorType type, Player whose)
+{
+	switch (type) {
+		case MirrorType::Left:
+			Mirror.Left.whose = whose;
+
+			//7/6 todo
+	}
+	return true;
+}
+
+template<int XNum, int YNum>
+inline ChessGrid& ChessMap<XNum, YNum>::GetGrid(int x, int y)
+{
+	return *data[y][x];
+}
+
+template<int XNum, int YNum>
+bool ChessMap<XNum, YNum>::init()
 {
 	for (RowType row : data) {
 		for (ChessGrid* grid : row) {
+			// TODO: init every grid
 		}
 	}
-	return false;
+	return true;
 }
 
-template<int ColNum, int RowNum>
-Player ChessDataImpl<ColNum, RowNum>::WhoWins()
+template<int XNum, int YNum>
+ChessGrid& ChessDataImpl<XNum, YNum>::GetGrid(int x, int y)
+{
+	return map.GetGrid(x, y);
+}
+
+template<int XNum, int YNum>
+inline bool ChessDataImpl<XNum, YNum>::init()
+{
+	return map.init();
+}
+
+template<int XNum, int YNum>
+Player ChessDataImpl<XNum, YNum>::WhoWins()
 {
 	return Player::None;
 }
 
-template<int ColNum, int RowNum>
-bool ChessDataImpl<ColNum, RowNum>::AddMirror(int col, int row, Player whose)
+template<int XNum, int YNum>
+bool ChessDataImpl<XNum, YNum>::AddMirror(int x, int y, MirrorType type, Player whose)
 {
-	return false;
+	GridType t=GetGrid(x, y).GetGridType();
+	switch (t) {
+		case GridType::Home:
+			//Error: can not place mirror on home
+			return false;
+		case GridType::Normal:
+			static_cast<ChessGridHome&>(GetGrid(x, y)).AddMirror(x, y, type, whose);
+	}
+	return true;
 }
--- Core/Core.h
+++ Core/Core.h
@@ -13,12 +13,28 @@ enum class Direction {
 	Left = 1,
 	Right = 2,
 	Top = 4,
-	Bottom = 8,
+	Bottom = 8
+};
+
+enum class MirrorType {
+	Left,
+	Right,
+	Top,
+	Bottom,
+	Slash,
+	BackSlash
+};
+
+enum class GridType {
+	Home,
+	Normal
 };
 
 class ChessGrid {
 public:
 	ChessGrid() = default;
+public:
+	virtual GridType GetGridType() = 0;
 };
 
 class ChessGridHome :public ChessGrid {
@@ -27,16 +43,20 @@ public:
 protected:
 	Player whose = Player::None;
 	Direction direction;
+public:
+	virtual GridType GetGridType()override;
 };
 
+
+
 class ChessGridNormal :public ChessGrid {
-public:
+protected:
 	struct{
-		struct{
+		struct {
 			Player whose;
-		}Left,Top,Right,Bottom;
-		struct{
-			enum class CrossType {
+		}Left, Top, Right, Bottom;
+		struct {
+			enum class PlaceOfCross {
 				None,
 				LeftTop_RightBottom,
 				LeftBottom_RightTop
@@ -44,7 +64,25 @@ public:
 			Player whose;
 		}Cross;
 	}Mirror;
+public:
+	virtual GridType GetGridType()override;
+	bool AddMirror(int x, int y, MirrorType type, Player whose);
 };
+
+
+template <int XNum, int YNum>
+class ChessMap {
+public:
+	ChessMap() = default;
+protected:
+	using RowType = std::array<ChessGrid*, XNum>;
+	using DataType = std::array<RowType, YNum>;
+	DataType data;
+public:
+	bool init();
+	ChessGrid& GetGrid(int x, int y);
+};
+
 
 class ChessData {
 public:
@@ -55,16 +93,17 @@ public:
 	virtual bool AddMirror(int col, int row, Player whose) = 0;
 };
 
-template <int ColNum, int RowNum>
+template <int XNum, int YNum>
 class ChessDataImpl :public ChessData {
-protected:
-	using RowType = std::array<ChessGrid*, ColNum>;
-	using DataType = std::array<RowType, RowNum>;
-	DataType data;
 public:
+	ChessDataImpl() = default;
+protected:
+	ChessMap<XNum, YNum> map;
+public:
+	virtual ChessGrid& GetGrid(int x, int y);
 	virtual bool init() override;
 	virtual Player WhoWins() override;
-	virtual bool AddMirror(int col, int row, Player whose) override;
+	virtual bool AddMirror(int x, int y, MirrorType type, Player whose) override;
 };
 
 int main() {
