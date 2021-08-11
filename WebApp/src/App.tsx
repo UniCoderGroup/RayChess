05daccb5588b072d73aa385aa93fe01834df0601
Kerman <KermanX@foxmail.com>
2021-08-11 13:20:44 +0800

    update

--- WebApp/src/App.tsx
+++ WebApp/src/App.tsx
@@ -5,14 +5,14 @@ import * as r from 'raychess-jscore';
 import { createJsxElement } from 'typescript';
 
 let GameType = r.TypeOfGame.Undefined;
-let GameData : r.Game = new r.Game;/*!!!*/
+let GameData: r.Game = new r.Game();/*!!!*/
 
-let me: r.Player ;
+GameData.InitBoard(6, 8);
 
 const ColorOfPlayer = new Map<r.Player, any>([
     [r.Player.P1, "blue"],
     [r.Player.P2, "red"],
-    [r.Player.None,"black"]
+    [r.Player.None, "black"]
 ]);
 
 const StringOfPlayer = new Map<r.Player, String>([
@@ -24,36 +24,35 @@ const StringOfPlayer = new Map<r.Player, String>([
 class Grid extends React.Component {
     render() {
         return (
-            <button className="grid">
+            <button className="game-grid">
             </button>
         );
     }
 }
 
 class Board extends React.Component {
-    renderGrid(x:number,y:number) {
+    renderGrid(grid: r.Grid) {
         return <Grid />;
     }
-    renderRow(y: number): JSX.Element{
-        let arr = new Array<JSX.Element>();
-        for (let x = 0; x < GameData.Nx; x++) {
-            arr.push(this.renderGrid(x, y));
-        }
-        return/* <div className="board-row" >*/{arr}/*</div>*/;
+    renderRow(y: number): JSX.Element[] {
+        return GameData.Board.Data[y].map((value, index) => {
+            return this.renderGrid(value);
+        });
     }
     render() {
-        const status = <span>Next player:<span style={{ color: ColorOfPlayer.get(r.Player.P1) }}>{ StringOfPlayer.get(r.Player.P1)}</span></span>;
-
-        let rows = new Array<JSX.Element>();
-        for (let y = 0; y < GameData.Ny; y++) {
-            rows.push(this.renderRow(y));
-        }
-        let board = <div className="board" >{rows}</div>;
+        let status = <span>Next player:<span style={{ color: ColorOfPlayer.get(r.Player.P1) }}>{StringOfPlayer.get(r.Player.P1)}</span></span>;
+        let board = GameData.Board.Data.map((value, index) => {
+            return (
+                <div className="game-row" key={index}>
+                    {this.renderRow(index)}
+                </div>
+            );
+        });
 
         return (
             <div>
                 <div className="status">{status}</div>
-                {board}
+                <div className="game-board" >{board}</div>
             </div>
         );
     }
@@ -63,9 +62,7 @@ class Game extends React.Component {
     render() {
         return (
             <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
+                <Board />
                 <div className="game-info">
                     <div>{/* status */}</div>
                     <ol>{/* TODO */}</ol>
