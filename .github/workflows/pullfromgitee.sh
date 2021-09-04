e38f346ff7036dafe5c521aa6aeffb700a42e036
Kerman <KermanX@foxmail.com>
2021-09-04 22:44:45 +0800

    upd

--- .github/workflows/pullfromgitee.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eux
-
-git clone https://gitee.com/uni_coder/raychess.git
-cd raychess
-git remote add github https://github.com/UniCoderGroup/RayChess.git
-git push github master -f
