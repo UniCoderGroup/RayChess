a8f6aa6a1acd9089dae9b594b8fa0de9f7a32fa2
Kerman <KermanX@foxmail.com>
2021-09-05 12:54:12 +0800

    update

---
+++ Scripts/push.sh
@@ -0,0 +1,13 @@
+echo "Enter Commit Message"
+read msg
+
+cd ../
+for repo in "CppCore JsCore WebApp Win32TestApp"
+do
+cd ./$repo
+git add .
+git commit -m $msg
+git push origin master
+cd ../
+done
+read
