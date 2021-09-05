12ae02d7009c0ac797f23016273ec3be6464f969
Kerman <KermanX@foxmail.com>
2021-09-05 12:54:49 +0800

    update

--- Scripts/push.sh
+++ Scripts/push.sh
@@ -1,13 +1,16 @@
 echo "Enter Commit Message"
 read msg
 
-cd ../
+cd "../"
 for repo in "CppCore JsCore WebApp Win32TestApp"
 do
-cd ./$repo
+cd "./$repo"
 git add .
 git commit -m $msg
 git push origin master
-cd ../
+cd "../"
 done
+git add .
+git commit -m $msg
+git push origin master
 read
