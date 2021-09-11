24ebf75db668fa65c14f788598ecf4a82f6bdca3
Kerman <KermanX@foxmail.com>
2021-09-12 00:07:06 +0800

    update. Finish GameRecord Storage

--- Scripts/push.sh
+++ Scripts/push.sh
@@ -6,12 +6,12 @@ for repo in {"CppCore","JsCore","WebApp","Win32TestApp"}
 do
 cd "./$repo"
 git add .
-git commit -m $msg
+git commit -m "$msg"
 git push
 cd "../"
 done
 git add .
-git commit -m $msg
+git commit -m "$msg"
 git push origin master
 
 echo "END"
